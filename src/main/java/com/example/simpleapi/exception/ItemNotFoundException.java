15a52349b46ed97420cf8370274ef98387a2146e
Teofil1 <teofilrozaluk@gmail.com>
2021-12-06 22:06:30 +0100

    change type of exception

--- src/main/java/com/example/simpleapi/exception/ItemNotFoundException.java
+++ src/main/java/com/example/simpleapi/exception/ItemNotFoundException.java
@@ -1,6 +1,6 @@
 package com.example.simpleapi.exception;
 
-public class ItemNotFoundException extends Exception {
+public class ItemNotFoundException extends RuntimeException {
 
     public ItemNotFoundException(String id) {
         super("Could not find item with id " + id);
